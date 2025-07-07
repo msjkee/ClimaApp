3bbdacacfd25db1aa405f3bb16635e8abf29af25
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-07-07 15:44:31 +0300

    try/catch is applied for getLocation() method. initState() is implemented, so 0 buttons for now

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -10,49 +10,52 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      print('Enable your location, please!');
-      return;
-    }
-
-    LocationPermission permission = await Geolocator.checkPermission();
-
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
+    try {
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        print('Enable your location, please!');
         return;
       }
-    }
 
-    if (permission == LocationPermission.deniedForever) {
-      print('Location is disabled forever! Try to fix it in your iOS settings!');
-      return;
-    }
+      LocationPermission permission = await Geolocator.checkPermission();
 
-    if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
-      final LocationSettings locationSettings = LocationSettings(
-        accuracy: LocationAccuracy.high,
-        distanceFilter: 100,
-      );
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return;
+        }
+      }
 
-      Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
-      print(position);
+      if (permission == LocationPermission.deniedForever) {
+        print('Location is disabled forever! Try to fix it in your iOS settings!');
+        return;
+      }
+
+      if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+        final LocationSettings locationSettings = LocationSettings(
+          accuracy: LocationAccuracy.high,
+          distanceFilter: 100,
+        );
+
+        Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+        print(position);
+      }
+    } catch (e) {
+      print(e);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              getLocation();
-            },
-            child: Text('Get Location')
-        )
-      )
+
     );
   }
 }
