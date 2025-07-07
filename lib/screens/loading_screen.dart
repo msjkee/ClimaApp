d61fc956b5c576da179e05a98a1d2d0f5fb7be3d
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-07-07 18:12:19 +0300

    Decoding a data with the help of JSON. Printing out a decoded temperature, id and city

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -26,11 +27,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    http.Response response = await http.get(Uri.parse('https://bulk.openweathermap.org/sample/'));
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodedData = jsonDecode(data);
+      
+      double temp = decodedData['main']['temp'];
+      int id = decodedData['weather'][0]['id'];
+      String city = decodedData['name'];
+
+      print('$city \n $id \n $temp');
     } else {
       print(response.statusCode);
     }
