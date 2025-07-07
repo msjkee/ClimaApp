acd5dea290a646fad0fe03e671b765ed40367a0a
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-07-07 17:41:47 +0300

    First attempts with API working in Dart. Tried to get and parse some data from random URL

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -14,6 +15,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -21,6 +23,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     await location.getCurrentLocation();
     print('Latitude: ${location.latitude}, \n Longitude: ${location.longitude}');
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse('https://bulk.openweathermap.org/sample/'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
